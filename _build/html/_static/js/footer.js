ad62ac61905e1d1739e8f1af4771c2ea3248028a
Joana Chavez <chavezlucero88@gmail.com>
2018-08-20 17:36:23 +0200

    last version 2.6.0

---
+++ _build/html/_static/js/footer.js
@@ -0,0 +1,27 @@
+document.write('<div class="rst-versions" data-toggle="rst-versions" role="note" aria-label="versions">\
+   <span class="rst-current-version" data-toggle="rst-current-version">\
+     <span class="fa fa-book"> Singularity Docs - Admin Guide </span>\
+     v: latest\
+     <span class="fa fa-caret-down"></span>\
+   </span>\
+   <div class="rst-other-versions">\
+     <dl>\
+       <dt>Versions</dt>\
+         <dd><a href="https://www.sylabs.io/guides/2.6.0/admin-guide/">latest</a></dd>\
+     </dl>\
+     <dl>\
+       <dt>Downloads</dt>\
+       <dd>\
+         <a href="https://www.sylabs.io/guides/2.6.0/admin-guide.pdf">Latest PDF</a>\
+       </dd>\
+     </dl>\
+     <dl>\
+       <dt>Do you want to contribute? </dt>\
+         <dd>\
+           <a href="https://github.com/singularityware/singularity-admindocs">Find us on GitHub!</a>\
+         </dd>\
+     </dl>\
+\
+   </div>\
+ </div>\
+');
